8e3b4d30d58183c727ec0d48900537b899ec31f9
ibuki777 <hanahana7802@gmail.com>
2020-03-16 10:42:40 +0900

    complete user resistoring

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_12_103614) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
